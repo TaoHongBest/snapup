757068450df63c8a8bf3fdc59bfaecd5a2ec9a57
NOTpanda <alexander113@126.com>
2018-10-25 16:35:28 +0100

    Page caching and URL caching.

--- src/main/java/com/taohong/snapup/controller/GoodsController.java
+++ src/main/java/com/taohong/snapup/controller/GoodsController.java
@@ -1,15 +1,23 @@
 package com.taohong.snapup.controller;
 
 import com.taohong.snapup.domain.SnapupUser;
+import com.taohong.snapup.redis.GoodsKey;
+import com.taohong.snapup.redis.KeyPrefix;
 import com.taohong.snapup.redis.RedisService;
 import com.taohong.snapup.service.GoodsService;
 import com.taohong.snapup.service.SnapupUserService;
 import com.taohong.snapup.vo.GoodsVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.spring4.context.SpringWebContext;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.util.StringUtils;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
@@ -28,23 +36,52 @@ public class GoodsController {
     @Autowired
     GoodsService goodsService;
 
+    @Autowired
+    ThymeleafViewResolver thymeleafViewResolver;
+
+    @Autowired
+    ApplicationContext applicationContext;
+
     /**
      * 288 QPS at HEAP:="-Xms2048m -Xmx2048m -Xss256k" (no exception)
      * 3000 * 10
      */
-    @RequestMapping("/to_list")
-    public String list(Model model, SnapupUser user) {
+    @RequestMapping(value = "/to_list", produces = "text/html")
+    @ResponseBody
+//    @RequestMapping("/to_list")
+    public String list(HttpServletRequest request, HttpServletResponse response, Model model, SnapupUser user) {
         model.addAttribute("user", user);
         // Query goods list
         List<GoodsVo> goodsList = goodsService.listGoodsVo();
         model.addAttribute("goodsList", goodsList);
-        return "goods_list";
+//        return "goods_list";
+        String html = redisService.get(GoodsKey.getGoodsList, "", String.class);
+        // Get cache
+        if (!StringUtils.isEmpty(html)) {
+            return html;
+        }
+        SpringWebContext ctx = new SpringWebContext(request, response, request.getServletContext(),
+                request.getLocale(), model.asMap(), applicationContext);
+        // Render manually
+        html = thymeleafViewResolver.getTemplateEngine().process("goods_list", ctx);
+        if (!StringUtils.isEmpty(html)) {
+            redisService.set(GoodsKey.getGoodsList, "", html);
+        }
+        return html;
     }
 
-    @RequestMapping("/to_detail/{goodsId}")
-    public String detail(Model model, SnapupUser user,
+    @RequestMapping(value = "/to_detail/{goodsId}", produces = "text/html")
+    @ResponseBody
+    public String detail(HttpServletRequest request, HttpServletResponse response, Model model, SnapupUser user,
                          @PathVariable("goodsId") long goodsId) {
         model.addAttribute("user", user);
+        // Get cache
+        String html = redisService.get(GoodsKey.getGoodsDetail, "" + goodsId, String.class);
+        if (!StringUtils.isEmpty(html)) {
+            return html;
+        }
+
+        // Render manually
         GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
         model.addAttribute("goods", goods);
 
@@ -68,6 +105,15 @@ public class GoodsController {
         model.addAttribute("snapupStatus", snapupStatus);
         model.addAttribute("remainSeconds", remainSeconds);
 
-        return "goods__detail";
+//        return "goods__detail";
+
+        SpringWebContext ctx = new SpringWebContext(request, response, request.getServletContext(),
+                request.getLocale(), model.asMap(), applicationContext);
+        // Render manually
+        html = thymeleafViewResolver.getTemplateEngine().process("goods__detail", ctx);
+        if (!StringUtils.isEmpty(html)) {
+            redisService.set(GoodsKey.getGoodsDetail, "" + goodsId, html);
+        }
+        return html;
     }
 }
