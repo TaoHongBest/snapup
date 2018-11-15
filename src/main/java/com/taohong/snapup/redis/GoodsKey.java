8c53119e660db6d41226645b5d269c3182660e26
NOTpanda <alexander113@126.com>
2018-11-15 22:56:34 +0000

    Now page refreshes like instantly after snap up.

--- src/main/java/com/taohong/snapup/redis/GoodsKey.java
+++ src/main/java/com/taohong/snapup/redis/GoodsKey.java
@@ -8,7 +8,7 @@ public class GoodsKey extends BasePrefix {
         super(expireSeconds, prefix);
     }
 
-    public static GoodsKey getGoodsList = new GoodsKey(60, "gl");
+    public static GoodsKey getGoodsList = new GoodsKey(1, "gl");
     public static GoodsKey getGoodsDetail= new GoodsKey(60, "gd");
     public static GoodsKey getSnapupGoodsStock= new GoodsKey(0, "gs");
 }
